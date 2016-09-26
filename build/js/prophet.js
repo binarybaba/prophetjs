077dc5a625b359fc464dfb1d51364308c08d2dfc
Amin Mohamed Ajani <me@allrightamin.xyz>
2016-09-27 00:43:17 +0530

    create visual stack of notifications

--- build/js/prophet.js
+++ build/js/prophet.js
@@ -4,12 +4,28 @@ if (!Date.now) {
 }
 var Message = (function () {
     function Message(message, id) {
-        this.message = message;
+        this.message = message ? message : 'Success!';
         this.id = id ? id : Message.idGen();
         //TODO: make stack of notifications and remove them
-        var elem = document.getElementById('message');
-        elem.innerText = this.message;
+        var toAttach = document.getElementById('prophet');
+        /*var toAttach = document.getElementsByTagName('body')[0];*/
+        var notification = document.createElement('li');
+        _a = ["message", this.message], notification.className = _a[0], notification.innerText = _a[1];
+        /*notification.innerText=this.message;*/
+        toAttach.appendChild(notification);
+        setTimeout(function () {
+            notification.classList.remove('active');
+            toAttach.removeChild(notification);
+        }, 3000);
+        notification.addEventListener('click', function () {
+            notification.classList.remove('active');
+            toAttach.removeChild(notification);
+        });
+        setTimeout(function () {
+            notification.classList.add('active');
+        }, 10);
         return this;
+        var _a;
     }
     Message.idGen = function () {
         return Date.now();
--- build/js/prophet.ts
+++ build/js/prophet.ts
@@ -2,21 +2,41 @@
 if (!Date.now) { Date.now = function now() { return new Date().getTime(); }; }
 
 class Message {
+    /*Todo: set min width of li elements and and find a way to animate the stack up by maybe
+    * Todo: setting storing the position and animate them up;
+    * Todo: Make notification fadeout in like 3 seconds after active configurable
+    * */
     id : number;
     message : string;
     type: string;
     constructor(message, id){
-        this.message = message;
+        this.message = message ? message : 'Success!';
         this.id = id ? id : Message.idGen();
         //TODO: make stack of notifications and remove them
-        var elem = document.getElementById('message');
-        
-        elem.innerText = this.message;
+        var toAttach = document.getElementById('prophet');
+        /*var toAttach = document.getElementsByTagName('body')[0];*/
+        var notification = document.createElement('li');
+        [notification.className, notification.innerText] = ["message", this.message];
+        /*notification.innerText=this.message;*/
+        toAttach.appendChild(notification);
+        setTimeout(function(){
+            notification.classList.remove('active');
+            toAttach.removeChild(notification);
+        },3000);
+        notification.addEventListener('click', function(){
+            notification.classList.remove('active');
+            toAttach.removeChild(notification);
+        });
+        setTimeout(function(){
+            notification.classList.add('active');
+        },10);
         return this;
     }
+
     static idGen(){
         return Date.now();
     }
+
     foo(){
         console.log(this.message, "-", this.id, "Chain Started in Foo");
         return this;
