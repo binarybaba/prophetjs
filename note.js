b9c81055be83d267c3bc2011266478e5d51714e4
Amin Mohamed Ajani <me@allrightamin.xyz>
2016-09-28 05:08:38 +0530

    dw

--- note.js
+++ note.js
@@ -1,35 +1,45 @@
 var styleList = [
-    { type : "foo", prop : "something foo"},
-    { type : "bar", prop : "something bar"}
+    { type : "foo", prop : "foo"},
+    { type : "bar", prop : "bar"}
 ];
 
 var custom = [
-    { type : "foo", prop : "something new foo" },
-    { type : "baz", prop : "something new baz"},
-    { type : "bar", prop : "something new baz"}
+    { type : "foo", prop : "new foo" },
+    { type : "baz", prop : "new baz"},
+    { type : "bar", prop : "new bar"}
 ];
 
 function find(objArr, keyToFind){
     var foundPos = objArr.map(function(preset){
         return preset.type;
     }).indexOf(keyToFind);
-    return foundPos === -1 ? false : foundPos;
+    return foundPos;
 }
 
 function set(custom){
-    for(var i = 0; i< custom.length; i++){
+    for(var i = 0, shared=false, current; i< custom.length; i++){
+        console.log("Finding", custom[i].type, "in styleList")
         var pos = find(styleList, custom[i].type);
-        if(pos){
+        current=custom[i];
+        if(pos !== -1) { console.log("Found at index", pos); shared = true;}
+        else {console.log("Not found"); }
 
-            //update the data at pos dry run please
-        }
     }
 
-    var s = find(objArr, "bam");
-    return s;
+    if (shared) {
+        for(var key in current){
+            console.log("SHARED BLOCK: Setting", styleList[pos], "'s ",styleList[pos][key], "to ", current[key]);
+            styleList[pos][key] = current[key];
+        }
+    }else{
+        console.log("NOT SHARED BLOCK: Setting styleList's last element to",current);
+        styleList[styleList.length] = current;
+    }
+
 }
 
-
 console.log(set(custom));
+/*console.log(find(styleList, "bar"))*/
+
 
 
